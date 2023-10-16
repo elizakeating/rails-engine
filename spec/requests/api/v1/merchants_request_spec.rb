9a12eca952c3a07f2f550386ba147d9f0415098d
Eliza Keating <ek.elizakeating@gmail.com>
2023-10-16 16:36:09 -0600

    test: add test for all items for a given merchant endpoint

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -36,4 +36,35 @@ describe "Merchants API" do
     expect(merchant).to have_key(:name)
     expect(merchant[:name]).to be_a(String)
   end
+
+  it "can get the items for a given merchant ID" do
+    merchant = create(:merchant)
+
+    merchant_items = create_list(:item, 3, merchant: merchant)
+    
+    get "/api/v1/merchants/#{merchant.id}/items"
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(items.count).to eq(3)
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(Integer)
+
+      expect(item).to have_key(:name)
+      expect(item[:name]).to be_a(String)
+
+      expect(item).to have_key(:description)
+      expect(item[:description]).to be_a(String)
+
+      expect(item).to have_key(:unit_price)
+      expect(item[:unit_price]).to be_a(Float)
+
+      expect(item).to have_key(:merchant_id)
+      expect(item[:merchant_id]).to be_an(Integer)
+    end
+  end
 end
