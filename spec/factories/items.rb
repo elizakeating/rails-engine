066bf52d858d81455a35bb0f65bffca7ff21ae8a
Eliza Keating <ek.elizakeating@gmail.com>
2023-10-16 16:13:02 -0600

    feat/test: add item model, add test for relationship to merchant, add relationship to merchant

---
+++ spec/factories/items.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :item do
+    name { "Item #{Faker::Lorem.sentence(word_count: 2)}" }
+    description { Faker::Lorem.paragraph(sentence_count: 3) }
+    unit_price { Faker::Number.between(from: 1.0, to: 1000.0) }
+  end
+end
