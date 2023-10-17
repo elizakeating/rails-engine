4c7b4ce7ef91abd2a0b47e92c143683524d53687
Eliza Keating <ek.elizakeating@gmail.com>
2023-10-16 18:11:21 -0600

    test: add tests for update item endpoint, destroy item endpoint, and get merchant data endpoint

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -78,4 +78,51 @@ describe "Items API" do
     expect(created_item.unit_price).to eq(item_params[:unit_price])
     expect(created_item.merchant_id).to eq(item_params[:merchant_id])
   end
+
+  it "can update an exisiting item" do
+    merchant = create(:merchant)
+    item = create(:item, merchant: merchant)
+    previous_name = Item.last.name
+    item_params = { name: "A brush for hair" }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    patch "/api/v1/items/#{item.id}", headers: headers, params: JSON.generate({item: item_params})
+    item = Item.find_by(id: item.id)
+
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq("A brush for hair")
+  end
+
+  it "can destroy an item" do
+    merchant = create(:merchant)
+    item = create(:item, merchant: merchant)
+
+    expect(Item.count).to eq(1)
+
+    delete "/api/v1/items/#{item.id}"
+
+    expect(response).to be_successful
+    expect(Item.count).to eq(0)
+    expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  it "can get the merchant data for a given item ID" do
+    merchant = create(:merchant)
+    item = create(:item, merchant: merchant)
+
+    get "/api/v1/items/#{item.id}/merchant"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_an(Integer)
+    expect(merchant[:id]).to eq(merchant[:id])
+
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_a(String)
+    expect(merchant[:name]).to eq(merchant[:name])
+  end
 end
