26978fec1203838fb04e90528f58b88dac6602bc
Eliza Keating <ek.elizakeating@gmail.com>
2023-10-16 18:13:19 -0600

    feat: add merchant items controller and index and show action

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::MerchantItemsController < ApplicationController
   def index
     render json: Merchant.find(params[:merchant_id]).items
   end
+
+  def show
+    render json: Item.find(params[:item_id]).merchant
+  end
 end
