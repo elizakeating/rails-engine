e6a90369551e234cf192c9ceca37e1ed1b2ea503
Eliza Keating <ek.elizakeating@gmail.com>
2023-10-16 18:11:44 -0600

    feat: add route to item merchant, change route to merchant items endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show] do
-        resources :items, only: [:index], controller: "merchant_items"
+        get "/items", to: "merchant_items#index"
       end
 
-      resources :items, only: [:index, :show, :create]
+      resources :items do
+        get "/merchant", to: "merchant_items#show"
+      end
     end
   end
 end
