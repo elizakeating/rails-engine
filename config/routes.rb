13c77c353907fc05117e275b7055377900502d49
Eliza Keating <ek.elizakeating@gmail.com>
2023-10-18 17:48:38 -0600

    feat: add route to items search controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  
   namespace :api do
     namespace :v1 do
+      get "/items/find", to: "items_search#show"
+
       resources :merchants, only: [:index, :show] do
         get "/items", to: "merchant_items#index"
       end
