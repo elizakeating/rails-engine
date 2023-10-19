41bd28c5d60d26d90b18daa15b4388999d3694ce
Eliza Keating <ek.elizakeating@gmail.com>
2023-10-19 00:14:03 -0600

    test: add test for both min and max price and sad path testing for a price and a name

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -194,4 +194,39 @@ describe "Items API" do
     expect(item[:data][:attributes][:description]).to eq(item_1.description)
     expect(item[:data][:attributes][:unit_price]).to eq(item_1.unit_price)
   end
+
+  it "can get first item sorted alphabeticaly more than the min price AND less than the max price searched" do
+    merchant = create(:merchant)
+    item_1 = Item.create!(name: "Hairbrush", description: "a brush to brush your hair", unit_price: 4.99, merchant_id: merchant.id)
+    item_2 = Item.create!(name: "Toothbrush", description: "a brush to brush your teeth", unit_price: 10.99, merchant_id: merchant.id)
+    item_3 = Item.create!(name: "How to utilize your brush", description: "a book about utilizing many brushes", unit_price: 15.99, merchant_id: merchant.id)
+    item_4 = Item.create!(name: "Paintbrush", description: "a brush to paint with", unit_price: 20.00, merchant_id: merchant.id)
+    item_5 = Item.create!(name: "hat", description: "wear on your head", unit_price: 25.00, merchant_id: merchant.id)
+
+    get "/api/v1/items/find?min_price=10.00&max_price=24.00"
+
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(item[:data][:id]).to eq("#{item_3.id}")
+    expect(item[:data][:type]).to eq("item")
+    expect(item[:data][:attributes][:name]).to eq(item_3.name)
+    expect(item[:data][:attributes][:description]).to eq(item_3.description)
+    expect(item[:data][:attributes][:unit_price]).to eq(item_3.unit_price)
+  end
+
+  it "gives an error if you try to find item with both name and price" do
+    merchant = create(:merchant)
+    item_1 = Item.create!(name: "Hairbrush", description: "a brush to brush your hair", unit_price: 4.99, merchant_id: merchant.id)
+    item_2 = Item.create!(name: "Toothbrush", description: "a brush to brush your teeth", unit_price: 10.99, merchant_id: merchant.id)
+    item_3 = Item.create!(name: "How to utilize your brush", description: "a book about utilizing many brushes", unit_price: 15.99, merchant_id: merchant.id)
+    item_4 = Item.create!(name: "Paintbrush", description: "a brush to paint with", unit_price: 20.00, merchant_id: merchant.id)
+    item_5 = Item.create!(name: "hat", description: "wear on your head", unit_price: 25.00, merchant_id: merchant.id)
+
+    get "/api/v1/items/find?name=bRu&max_price=24.00"
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+  end
 end
