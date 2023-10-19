9d39c1c4d8eca20b368b8bb17df042bbbfbfa34f
Eliza Keating <ek.elizakeating@gmail.com>
2023-10-19 00:14:12 -0600

    feat: add logic for if name and price is sent

--- app/controllers/api/v1/items_search_controller.rb
+++ app/controllers/api/v1/items_search_controller.rb
@@ -1,10 +1,19 @@
 class Api::V1::ItemsSearchController < ApplicationController
   def show
-    if !params[:name].nil?
+    if (!params[:min_price].nil? && !params[:name].nil?) || (!params[:max_price].nil? && !params[:name].nil?)
+      render status: 400
+    elsif !params[:name].nil?
       query = params[:name]
 
       item = Item.where("lower(name) like lower('%#{query}%')").order("lower(name)").first
     
+      render json: ItemSerializer.new(item)
+    elsif !params[:min_price].nil? && !params[:max_price].nil?
+      max_price = params[:max_price]
+      min_price = params[:min_price]
+
+      item = Item.where("unit_price >= ? AND unit_price <= ?", min_price, max_price).order("lower(name)").first
+
       render json: ItemSerializer.new(item)
     elsif !params[:min_price].nil? 
       query = params[:min_price]
