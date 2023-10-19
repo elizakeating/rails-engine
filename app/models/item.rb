61041541a410feb53a9653b3d447238b6a29cbb6
Eliza Keating <ek.elizakeating@gmail.com>
2023-10-19 15:40:58 -0600

    feat: add max_price item model method and adjust controller accordingly

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,14 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :unit_price, presence: true
   validates :merchant_id, presence: true
+
+  def self.max_price(query)
+    item = Item.where("unit_price <= ?", query).order("lower(name)").first
+
+    if item.nil?
+      item = Item.new
+    else
+      item
+    end
+  end
 end
