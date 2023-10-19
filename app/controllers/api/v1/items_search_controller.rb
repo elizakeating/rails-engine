33d06986c3b3d99a52988e090d95ac7a15ac35e0
Eliza Keating <ek.elizakeating@gmail.com>
2023-10-19 09:13:10 -0600

    feat: add logic to handle min price less than 0

--- app/controllers/api/v1/items_search_controller.rb
+++ app/controllers/api/v1/items_search_controller.rb
@@ -15,12 +15,16 @@ class Api::V1::ItemsSearchController < ApplicationController
       item = Item.where("unit_price >= ? AND unit_price <= ?", min_price, max_price).order("lower(name)").first
 
       render json: ItemSerializer.new(item)
-    elsif !params[:min_price].nil? 
+    elsif !params[:min_price].nil?
       query = params[:min_price]
 
-      item = Item.where("unit_price >= ?", query).order("lower(name)").first
+      if query.to_f < 0.00
+        render json: { errors: ["minimum price less than 0"]}, status: 400
+      else
+        item = Item.where("unit_price >= ?", query).order("lower(name)").first
 
-      render json: ItemSerializer.new(item)
+        render json: ItemSerializer.new(item)
+      end
     elsif !params[:max_price].nil?
       query = params[:max_price]
 
